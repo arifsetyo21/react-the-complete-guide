3b7d44a17a0e91b24ec2136d8a064c4bee457cb3
arifsetyo21 <arifsetyo19@gmail.com>
2021-09-07 16:43:53 +0700

    64. Using Stateful Lists
    - In this lessons, we using previous value from state to update the state
    - We handle addExpenseHandler method to update the expense list

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState("2020");
+
+  const addFilterExpensesHandler = (selectedYear) => {
+    console.log("filtered from Expenses");
+    setFilterYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filterYear}
+        onChangeFilter={addFilterExpensesHandler}
+      />
       {props.expenses.map((expense) => (
         <ExpenseItem
           title={expense.title}
