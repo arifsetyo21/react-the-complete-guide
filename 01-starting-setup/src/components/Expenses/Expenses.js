ac6bad254c4821e79374a8b4da8cf19e1c9f7526
arifsetyo21 <arifsetyo19@gmail.com>
2021-09-08 10:01:56 +0700

    70. Wrap up & Next Steps
    - In this lesson, we learn about how to make chartbar to the app

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpenseChart";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2020");
@@ -18,14 +18,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filterYear}
         onChangeFilter={addFilterExpensesHandler}
       />
+      <ExpenseChart expense={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
