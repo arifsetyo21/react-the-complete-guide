a82f49eea0e712a8281432763c684c64849c18e2
arifsetyo21 <arifsetyo19@gmail.com>
2021-09-16 17:46:56 +0700

    191. Using the custom http hook
    - To implement custom http hook to application, we need suite the code of application with the hook

--- 15-onwards-to-a-more-realistic-example/src/components/hooks/use-http.js
+++ 15-onwards-to-a-more-realistic-example/src/components/hooks/use-http.js
@@ -11,9 +11,9 @@ const useHttp = (requestConfig, applyData) => {
       const response = await fetch(
         requestConfig.url,
         {
-          method: requestConfig.method,
-          headers: requestConfig.headers,
-          body: JSON.stringify(requestConfig.body),
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
         }
         //   "https://react-http-udemy-8a88f-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
       );
