60228eee8d8036ed14a085a05dac0a0c41b8c0ba
arifsetyo21 <arifsetyo19@gmail.com>
2021-09-17 10:02:33 +0700

    200. Providing validation feedback
    - To inform to user is any invalid input, we need to feedback to user
    - So we can using state that is any invalid form input, when invalid we can set to false value, when valid we can set to true value

--- 16-starting-project/src/components/SimpleInput.js
+++ 16-starting-project/src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,17 +13,24 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
+
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -32,6 +40,7 @@ const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
+      {!enteredNameIsValid && <p>Name must not be empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
