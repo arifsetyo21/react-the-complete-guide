35fd80dc970f08c6972f404b8fa1d45b9833bd74
arifsetyo21 <arifsetyo19@gmail.com>
2021-09-28 11:32:07 +0700

    259. Getting started with fetching data
    - To get data from firebase, we can fetch data
    - So when we load app for the first time, we can get data from firebase, then when we have done fetch data, we can replace cart data
    - to do this, we can using useEffect to trigger action

--- 19-starting-project/src/store/cart-slice.js
+++ 19-starting-project/src/store/cart-slice.js
@@ -44,51 +44,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://react-http-udemy-8a88f-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-    };
-
-    try {
-      await sendRequest();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data sucessfully!",
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending data failed!",
-        })
-      );
-    }
-  };
-};
-
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
