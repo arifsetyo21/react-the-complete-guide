b5cb485e8875fcf9de68868dd2b8ab2db3b3d8ea
arifsetyo21 <arifsetyo19@gmail.com>
2021-10-05 11:42:14 +0700

    285. Getting creative with nested routes
    - To show and hide a component, we also can use nested route, so when we visit exact path, we show or hide that element

--- 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -4,6 +4,7 @@ import { Route, useParams } from "react-router";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import Comments from "../components/comments/Comments";
+import { Link } from "react-router-dom";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
@@ -22,6 +23,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
