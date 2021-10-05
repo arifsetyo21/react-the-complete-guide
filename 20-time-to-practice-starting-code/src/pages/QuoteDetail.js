7f40a24f7d302de5e50334750ae0596ccb3c6d8b
arifsetyo21 <arifsetyo19@gmail.com>
2021-10-05 23:01:00 +0700

    286. Writing more flexible routing code
    - To handle routing with more dynamic changing, and less harcode we can using useRouteMatch

--- 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router";
+import { Route, useParams, useRouteMatch } from "react-router";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -13,6 +13,9 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
+
+  console.log(match);
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -23,14 +26,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
