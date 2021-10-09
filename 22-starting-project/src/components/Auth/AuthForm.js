30ebd71da85c7a3cca42e20670be98aa14efeae7
arifsetyo21 <arifsetyo19@gmail.com>
2021-10-10 00:05:21 +0700

    304. Managing the auth state with context
    - To save state or token after we successfuly logged in, we can store token to context or redux

--- 22-starting-project/src/components/Auth/AuthForm.js
+++ 22-starting-project/src/components/Auth/AuthForm.js
@@ -1,10 +1,14 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useContext } from "react";
 
 import classes from "./AuthForm.module.css";
+
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -60,7 +64,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((error) => {
         alert(error.message);
